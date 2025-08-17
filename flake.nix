769e387d41a3f4d19d0cf0fd7081a5470c7641ae
Aleksey Zhukov <353748+alezkv@users.noreply.github.com>
2025-08-17 17:08:17 +0200

    lock nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Gemini CLI package";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/32f313e49e42f715491e1ea7b306a87c16fe0388";
   };
 
   outputs = { self, nixpkgs }:
